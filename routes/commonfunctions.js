39a72cf84f6ffbe1fa9496a8e312b054ce6137d7
Rohan Kumar <rohankanojia420@gmail.com>
2016-06-16 14:35:19 +0530

    added code for sending push notifications

--- routes/commonfunctions.js
+++ routes/commonfunctions.js
@@ -1,5 +1,26 @@
+/**
+ * @module Commonfunctions
+ */
+/////////////////////////////////////////////////////////////////////////
+// REQUIRED MODULES
+/////////////////////////////////////////////////////////////////////////
 
-exports.checkBlank = function(arr)
+var gcm            = require('node-gcm');
+var request        = require('request');
+var apns           = require('apn');
+
+exports.checkBlank                     = checkBlank;
+exports.sendIosPushNotification        = sendIosPushNotification;
+exports.sendAndroidPushNotification    = sendAndroidPushNotification;
+exports.sendNotification               = sendNotification;
+exports.sendNotificationToDevice       = sendNotificationToDevice;
+
+/**
+ * Function to check missing parameters in the API.
+ * @param arr
+ * @returns {number}
+ */
+function checkBlank(arr)
 {
     var arrlength = arr.length;
     for (var i = 0; i < arrlength; i++)
@@ -13,4 +34,132 @@ exports.checkBlank = function(arr)
     }
 
     return 0;
-};
+}
+
+/**
+ * function for sending push notifications for iOS device
+ * @param iosDeviceToken - device token of that particular device
+ * @param message - message to be sent
+ * @param flag - flag for it
+ * @param payload - payload
+ */
+function sendIosPushNotification(iosDeviceToken, message, flag, payload) {
+  var status = 1;
+  var msg = message;
+  var snd = 'ping.aiff';
+  if(flag == 2 || flag == 4 || flag == 6) {
+    status = 0;
+    msg = '';
+    snd = '';
+  }
+
+  var options = {
+    cert        : __dirname + '/../certs/' + config.get('iosApnCertificate'),
+    certData    : null,
+    key         : __dirname + '/../certs/' + config.get('iosApnCertificate'),
+    keyData     : null,
+    passphrase  : 'vevsa',
+    ca          : null,
+    pfx         : null,
+    pfxData     : null,
+    gateway     : 'gateway.push.apple.com',
+    port        : 2195,
+    rejectUnauthorized: true,
+    enhanced    : true,
+    cacheLength : 100,
+    autoAdjustCache: true,
+    connectionTimeout: 0,
+    ssl         : true
+  };
+
+  var deviceToken = new apns.Device(iosDeviceToken);
+  var apnsConnection = new apns.Connection(options);
+  var note = new apns.Notification();
+
+  note.expiry = Math.floor(Date.now() / 1000) + 3600;
+
+  note.sound = snd;
+  note.alert = msg;
+  note.newsstandAvailable = status;
+  note.payload = payload;
+
+  apnsConnection.pushNotification(note, deviceToken);
+
+  // Handle these evenet to confirm that the notification gets
+  // transmitted to the APN server or find error if any
+  function log(type) {
+    return function() {
+      if(debugging_enabled)
+        console.log("iOS PUSH NOTIFICATION RESULT: " + type);
+    }
+  }
+
+  apnsConnection.on('error', log('error'));
+  apnsConnection.on('transmitted', log('transmitted'));
+  apnsConnection.on('timeout', log('timeout'));
+  apnsConnection.on('connected', log('connected'));
+  apnsConnection.on('disconnected', log('disconnected'));
+  apnsConnection.on('socketError', log('socketError'));
+  apnsConnection.on('transmissionError', log('transmissionError'));
+  apnsConnection.on('cacheTooSmall', log('cacheTooSmall'));
+}
+
+/**
+ * Send push notification to android device
+ * @param deviceToken
+ * @param message
+ */
+function sendAndroidPushNotification(deviceToken, message) {
+  var message = new gcm.Message({
+    delayWhileIdle: false,
+    timeToLive: 2419200,
+    data: {
+      message: message,
+      brand_name: "Vevsa",
+    }
+  });
+  var sender = new gcm.Sender(constants.serverAndroidIDs.PUSH_NOTIFICATION_SERVER_ID);
+  var registrationIds = [];
+  registrationIds.push(deviceToken);
+
+  sender.send(message, resigtrationIds, 4, function(err, result) {
+    // explicitly freeing objects
+    sender = null;
+    message = null;
+  });
+}
+
+/**
+ *
+ * Send notification to the user with the given user ID
+ * ASSUMPTION: the payload is same for both the devices
+ * @param user_id
+ * @param message
+ * @param flag
+ * @param payload
+ */
+function sendNotification(user_id, message, flag, payload) {
+  console.log("SENDING NOTIFICATION: "+ message + " TO: "+ user_id);
+  var getUserDeviceInfo = "SELECT user_id, device_type, user_device_token FROM tb_users WHERE user_id = ?";
+  connection.query(getUserDeviceInfo, [user_id], function(err, resultUser) {
+    sendNotifictionToDevice(resultUser[0].device_type, resultUser[0].user_device_token, message, flag, payload);
+  });
+}
+
+/**
+ * Function to send notification to a particular device
+ * @param deviceType
+ * @param userDeviceToken
+ * @param message
+ * @param flag
+ * @param payload
+ */
+function sendNotificationToDevice(deviceType, userDeviceToken, message, flag, payload) {
+  if(deviceType == constants.deviceType.ANDROID && userDeviceToken != '') {
+    sendAndroidPushNotification(userDeviceToken, payload);
+  }
+  else if(deviceType == constants.deviceType.iOS && userDeviceToken != '') {
+    sendIosPushNotification(userDeviceToken, message, flag, payload);
+  }
+}
+
--- routes/constants.js
+++ routes/constants.js
@@ -1,3 +1,6 @@
+/**
+ * @module constants
+ */
 function define(obj, name, value) {
     Object.defineProperty(obj, name, {
         value:        value,
@@ -8,11 +11,18 @@ function define(obj, name, value) {
 }
 
 exports.responseFlags = {};
-define(exports.responseFlags, "ACTION_FAILED",       144);
-define(exports.responseFlags, "ACTION_COMPLETE",     143);
+define(exports.responseFlags, "ACTION_FAILED"       , 144);
+define(exports.responseFlags, "ACTION_COMPLETE"     , 143);
 
 exports.databaseErrorResponse = {
   "log": "Server execution error",
   "flag": 144
 };
+
+exports.deviceType = {};
+define(exports.deviceType, "ANDROID"                 , 1);
+define(exports.deviceType, "iOS"                     , 2);
+
+exports.serverAndroidIDs = {};
+define(exports.PUSH_NOTIFICATION_SERVER_ID           , 1);
 
