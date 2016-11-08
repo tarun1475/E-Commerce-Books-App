7db557cf2d35e8ef1074c6be0712372d629b006b
Tarun Gupta <tarunkumargupta14@gmail.com>
2016-11-08 20:04:18 +0000

    book_requests.js

--- routes/book_requests.js
+++ routes/book_requests.js
@@ -471,7 +471,7 @@ function confirmBookOrder(req, res) {
           html += "</tr>";
         }
         html += "<tr><td colspan=3 align=center><b>Urgent Delivery Charges</b></td><td align=center><b> Rs."+urgentDeliveryCharges+"</b></td>";
-        html += "<tr><td colspan=3 align=center><b>Total Price</b></td><td align=center><b> Rs."+totPrice+"</b></td>";
+        html += "<tr><td colspan=3 align=center><b>Total Price</b></td><td align=center><b> Rs."+(totPrice+urgentDeliveryCharges)+"</b></td>";
         html += "</table><br><br>";
 
         html += "These would be delivered to :<br><b>"+userName+",<br>"+deliveryAddress+"<br>"+userPhone+"</b>";
