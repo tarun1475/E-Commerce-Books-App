4b3e8f5f1c3945466316514051404a55ff4baea8
Tarun Gupta <tarunkumargupta14@gmail.com>
2016-11-08 20:19:48 +0000

    book_requests.js

--- routes/book_requests.js
+++ routes/book_requests.js
@@ -435,9 +435,13 @@ function confirmBookOrder(req, res) {
     }
     if(reqStatus == constants.bookRequestStatus.APPROVED) { 
       var totalPrice = 0;
+      var  urgentDeliveryCharges = 30;
       for(var i = 0; i < responseData.length; i++)
         totalPrice += responseData[i].price;
-      if(isUrgent == 1) totPrice += 30;
+      if(isUrgent == 1) {
+       totPrice = totPrice + urgentDeliveryCharges;
+      }
+      else  urgentDeliveryCharges = 0 ;
       deliverBooksToUser(handlerInfo, requestId, userId, deliveryAddress, isUrgent, responseData, function(delErr, delRes) {
         if(delErr) {
           return res.send({
