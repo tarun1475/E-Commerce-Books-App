178f65d4a2eea8295e5e82431bcacf75bbe32924
Tarun Gupta <tarunkumargupta14@gmail.com>
2016-12-22 19:57:47 +0000

    version changed

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,7 @@ function checkVersion(req, res) {
     "apiHandler":"checkVersion"
   };
   var version       = parseInt( req.body.app_version);
-  var appVersion    = 3;
+  var appVersion    = 2;
   if(version === appVersion){
     res.send({
       "log" : "Version matched",
