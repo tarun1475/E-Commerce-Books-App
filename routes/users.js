c961e2d479c964add7a581d36996f3462697f340
Tarun Gupta <tarunkumargupta14@gmail.com>
2017-02-16 08:09:15 +0000

    vevsa money funciton changed

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,7 @@ function vevsaMoney(req, res) {
     }
     res.send({
       "log" : "Vevsa Money fetched successfully",
-      "vevsa_money": result,
+      "vevsa_money": result.vevsa_money[0].vevsa_money,
       "flag": constants.responseFlags.ACTION_COMPLETE
     });
   });
