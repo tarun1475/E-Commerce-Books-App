29d06e9b2947110bfea38b8ee696aa251dd88d6e
Tarun Gupta <tarunkumargupta14@gmail.com>
2017-05-17 06:14:00 +0000

    put books to cart

--- app.js
+++ app.js
@@ -216,6 +216,9 @@ app.post('/req_book_auth/put_super_vendor_response'          , utils.logRequest
    , utils.verifyClientToken
    , requests.putBookRequestSuperVendorResponse
    , error);
+app.post('/req_book_auth/put_books_cart'          , utils.logRequest
+   , requests.putBooksToCart
+   , error);
 app.get('/req_book_auth/fetch_book_db'          , utils.logRequest
    , requests.fetchBooksDb
    , error);
