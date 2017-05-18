52b56773db383c5ac762480dce5cd90069470196
Tarun Gupta <tarunkumargupta14@gmail.com>
2017-05-18 10:59:17 +0000

    app.js edited online with Bitbucket

--- app.js
+++ app.js
@@ -216,6 +216,9 @@ app.post('/req_book_auth/put_super_vendor_response'          , utils.logRequest
    , utils.verifyClientToken
    , requests.putBookRequestSuperVendorResponse
    , error);
+app.get('/req_book_auth/member_ship'          , utils.logRequest
+   , requests.memberShip
+   , error);
 app.post('/req_book_auth/remove_cart_items'          , utils.logRequest
    , requests.removeCartItems
    , error);
