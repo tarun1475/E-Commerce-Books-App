4e6bc881575cf26ff3d1752c5ac51842b2b3305b
Tarun Gupta <tarunkumargupta14@gmail.com>
2017-05-22 15:05:08 +0000

    app.js edited online with Bitbucket

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ app.get('/books-auth/my_cart_count_orders'                , utils.logRequest
 app.get('/books-auth/my_cart_orders'                , utils.logRequest
   , users.getMyCartOrders
   , error);
-app.post('/books-auth/insert_code_vevsa_contest'                , utils.logRequest
+app.get('/books-auth/insert_code_vevsa_contest'                , utils.logRequest
   , users.insertCodeVevsaContest
   , error);
 app.post('/books-auth/vevsa_contest_user_details'                , utils.logRequest
