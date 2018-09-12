8941de1d59a76bbae054954b0b2303783d884918
tarun1475 <tarunkumargupta14@gmail.com>
2018-09-12 20:11:28 +0530

    oka

--- routes/keyUsers.js
+++ routes/keyUsers.js
@@ -434,39 +434,25 @@ function sendRecoveryTrustData(req, res) {
     if(err) {
       return res.send(constants.databaseErrorResponse);
     }
-
     request_id = result.insertId;
-
-   
-      // res.send({
-      //     "log": "User verified",
-      //     "flag": constants.responseFlags.ACTION_COMPLETE
-      //   });
-
    });
 
-     console.log(request_id);
+    for(i = 0 ; i < trustData.length ; i++){
 
-  
+      logRequestDetails(handlerInfo, request_id ,trustData[i].user_public_key ,function(userErr,userRes){
+      if(userErr)   return res.send(constants.databaseErrorResponse);
 
 
-
-    // for(i = 0 ; i < 1 ; i++){
-
-    //   logRequestDetails(handlerInfo, request_id ,trustData[i].user_public_key ,function(userErr,userRes){
-    //   if(userErr)   return res.send(constants.databaseErrorResponse);
-    //   console.log(i);
-
-    //    res.send({
-    //       "log": "User verified",
-    //       "flag": constants.responseFlags.ACTION_COMPLETE
-    //     });
+       res.send({
+          "log": "User verified",
+          "flag": constants.responseFlags.ACTION_COMPLETE
+        });
 
     
-    // });
+    });
 
 
-    // }
+    }
 
      
 
