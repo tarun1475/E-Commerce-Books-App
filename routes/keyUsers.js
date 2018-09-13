9a3c309a615441bed485c2a605334f2a601f004e
tarun1475 <tarunkumargupta14@gmail.com>
2018-09-13 20:44:56 +0530

    oka

--- routes/keyUsers.js
+++ routes/keyUsers.js
@@ -486,23 +486,17 @@ function fetchRecoveryRequests(req, res) {
   var resultArr       = [];
   var requestDetails  = [];
 
-  var requestArr = [];
 
 
+  function firstArr(callback){
     fetchNewRequestsFromDb(handlerInfo,publicKey,  function(err, result) {
     if(err) {
       return res.send(constants.databaseErrorResponse);
     }
 
-    requestArr = result;
-    
-    
-    });
 
-
-  function firstArr(callback){
-     for(i =0 ; i < requestArr.length ;i++){
-      fetchRecoveryRequestsDetails(handlerInfo,requestArr[i].request_id,function(reqErr , reqRes){
+    for(i =0 ; i < result.length ;i++){
+      fetchRecoveryRequestsDetails(handlerInfo,result[i].request_id,function(reqErr , reqRes){
          if(reqErr) {
           return res.send(constants.databaseErrorResponse);
         }
@@ -511,6 +505,9 @@ function fetchRecoveryRequests(req, res) {
 
       });
     }
+    
+    callback(null , result);
+  });
   }
 
   function secondArr(callback){
